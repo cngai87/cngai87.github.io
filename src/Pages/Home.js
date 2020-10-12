042d9b22caa01adc9d2017e4e86e058e30660f62
cngai87 <54323848+cngai87@users.noreply.github.com>
2020-10-12 16:22:40 -0400

    Edited text

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -86,11 +86,11 @@ export default function Home() {
 
                         <Section title={"About"} />
                         <p>
-                            Hi! Welcome to my webpage! This is my first forage into responsive website design so please be gentle. I'm 32-years old, living in Toronto and currently working in I.T. providing administration and support. I've been in this field for almost a decade now, and feeling too comfortable. Looking for more to do, and having many friends and family as developers, I figured I'd give software development a try! I've always been intrigued and drawn to programming since I first took a Java course in high school. Thinking it would be more useful to have some broader knowledge for jobs though, I decided to go into Electrical Engineering instead. During my university summers, I worked at a small company in their I.T. department. It was this experience that pushed me towards the I.T. field, but now I am back to what I had a passion for during high school, developing software.
+                            Hi! Welcome to my webpage! This is my first forage into responsive website design so please be gentle. I'm 32 years old, living in Toronto and currently working in the I.T. field. I've been in this field for almost a decade now, and feeling too comfortable. Looking for more to do, and having many friends and family as developers, I figured I'd give software development a try! I've always been intrigued and drawn to programming since I first took a Java course in high school. Thinking it would be more useful to have some broader knowledge for jobs though, I decided to go into Electrical Engineering instead. During my university summers, I worked at a small company in their I.T. department. It was this experience that pushed me towards the I.T. field, but now I am back to what I had a passion for during high school, developing software.
                         </p>
                         <h4>Javascript Web Developer</h4>
                         <p>
-                            Over the past year, I've been slowly been learning more and more Javascript. As the world becomes increasingly connected, with everyone on their phones constantly, I figured starting at the basics would be best. Using FreeCodeCamp's excellent learning platform, I began my journey with HTML, CSS and lastly Javascript. I have since picked up the library, React and started different projects just to get a better grip on all the new concepts, libraries, packages, and tools that I was being introduced to. Since then I have created a few projects, some that are still (and probably always will be) a work-in-progress. You can see them below in the <a href="#Portfolio">Portfolio</a> section.
+                            Over the past year, I've slowly been learning Javascript. As the world becomes increasingly connected, and with everyone on their phones constantly, I figured starting at the basics would be best. Using FreeCodeCamp's excellent learning platform, I began my journey with HTML, CSS and lastly Javascript. I have since picked up the library React, and have started different projects just to get a better grip on all the new concepts, libraries, packages, and tools that I was being introduced to. Since then I have created a few projects, some that are still (and probably always will be) a work-in-progress. You can see them below in the <a href="#Portfolio">Portfolio</a> section.
                         </p>
                         <h4>
                             I.T. Technician/Administrator
@@ -173,44 +173,11 @@ export default function Home() {
                     <Container>
                         <Section title={"Interests"} />
                         <p>
-                            While work keeps me indoors for most of my day, I try to get outside as much as possible. I am the owner of a goldendoodle dog, Brandy, so that definitely helps with making sure I get some fresh air everyday. During the summer and fall, I'm an avid hiker, and love to get to the cottage as often as I can to swim and canoe. During the winter, I'm an intermediate skiier with hopes to take on more advanced hills!
+                            While work keeps me indoors for most of my day, I try to get outside as much as possible. I am the owner of a Goldendoodle dog, Brandy, so that definitely helps with making sure I get some fresh air everyday. During the summer and fall, I'm an avid hiker, and love to get to the cottage as often as I can to swim and canoe. During the winter, I'm an intermediate skiier with hopes to take on more advanced hills!
                         </p>
                         <p>
                             While indoors, I enjoy exploring new technology whether it is fiddling with home automation, setting up security cameras, or creating websites and apps. I am also an avid home cook, always looking up new recipes to cook, or bake. I especially love to take on large group meals due to the hustle, time-management and personal responsibilty needed for a successful payoff. To relax, I either dig into some books with fantasy being my go-to genre, or watch some drama or Sci-Fi shows on TV. I also try to continue with my professional development by either working on a web-dev project or studying for a new certification.
                         </p>
-                        {/*}
-                        <Row className="mb-4">
-                            <Col className="interestsCol">
-                                <span>
-                                    <i className="fas fa-chevron-right mr-2" />
-                                    Dogs!
-                            </span>
-                            </Col>
-                            <Col className="interestsCol">
-                                <i className="fas fa-chevron-right mr-2" />
-                                Hiking
-                            </Col>
-                        </Row>
-                        <Row className="mb-4">
-                            <Col className="interestsCol">
-                                <i className="fas fa-chevron-right mr-2" />
-                                Fantasy Novels
-                            </Col>
-                            <Col className="interestsCol">
-                                <i className="fas fa-chevron-right mr-2" />
-                                Sci-Fi Media
-                            </Col>
-                        </Row>
-                        <Row>
-                            <Col className="interestsCol">
-                                <i className="fas fa-chevron-right mr-2" />
-                                Cooking
-                            </Col>
-                            <Col className="interestsCol">
-                                <i className="fas fa-chevron-right mr-2" />
-
-                            </Col>
-                        </Row> */}
                     </Container>
                 </section>
             </main>
